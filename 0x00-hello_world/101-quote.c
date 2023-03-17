f521b5b7f83f7adaad64ef74970a3a3ae5c804d6
Beyond-Logic <edemaero@gmail.com>
2023-03-17 02:03:03 +0000

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		putchar(str[i]);
+	}
+
+	return (1);
+}
