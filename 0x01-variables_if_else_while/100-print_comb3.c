0c5772724e0f575e20e6245a25f0644182be64a0
Beyond-Logic <edemaero@gmail.com>
2023-03-17 20:27:14 +0000

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 89; i++)
+	{
+		putchar((i / 10) + '0');
+		putchar((i % 10) + '0');
+
+		if (i <= 88)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar ('\n');
+
+	return (0);
+
+}
