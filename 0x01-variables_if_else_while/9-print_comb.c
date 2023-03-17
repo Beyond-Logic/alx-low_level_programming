131434e8578d769b185c192a4c0c6625887ba4bc
Beyond-Logic <edemaero@gmail.com>
2023-03-17 18:16:20 +0000

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success);
+*/
+
+int main(void)
+{
+	char z;
+
+	for (z = 0; z <= 9; z++)
+	{
+		putchar(z + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
