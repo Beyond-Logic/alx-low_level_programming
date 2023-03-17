9a07b92905079ccc3291a17ca9189471fbf00177
Beyond-Logic <edemaero@gmail.com>
2023-03-17 15:30:40 +0000

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,20 +2,20 @@
 
 /**
   * main - Entry Point
-  * 
+  *
   * Return: Always 0 (Success)
 */
 
 int main(void)
 {
 	char alphabet;
-	
-	for(alphabet ='a'; alphabet <= 'z'; alphabet++)
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		putchar(alphabet);
 	}
 	putchar('\n');
-	
+
 	return (0);
 
 }
