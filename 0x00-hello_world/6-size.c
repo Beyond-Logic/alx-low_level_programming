52f0519c2f7521989af8aab6204e872e2afaaa7c
Beyond-Logic <edemaero@gmail.com>
2023-03-17 01:07:17 +0000

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char c;
+
+	int i;
+
+	long int l;
+
+	long long int ll;
+
+	float f;
+
+	printf("size of a char: %lu byte(s)\n", sizeof(c));
+	printf("size of an int: %lu bytes(s)\n", sizeof(i));
+	printf("size of a long int: %lu bytes(s)\n", sizeof(l));
+	printf("size of a long long int: %lu bytes(s)\n", sizeof(ll));
+	printf("size of a float: %lu bytes(s)\n", sizeof(f));
+
+	return (0);
+
+}
