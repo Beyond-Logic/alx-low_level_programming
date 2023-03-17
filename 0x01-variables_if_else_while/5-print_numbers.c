b11b4d3ee94874401449f6f0001e0dedf33aba6a
Beyond-Logic <edemaero@gmail.com>
2023-03-17 16:38:07 +0000

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int b;
+
+	for (b = 0; b <= 9; b++)
+	{
+		printf("%d", b);
+	}
+	printf("\n");
+
+	return (0);
+
+}
