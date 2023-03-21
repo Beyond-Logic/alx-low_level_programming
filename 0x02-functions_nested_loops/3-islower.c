aa10a4e12899c78bf69aff6d7b47c478410e5904
Beyond-Logic <edemaero@gmail.com>
2023-03-21 23:44:08 +0000

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * is_lower - check if alphabet is lowercase
+ * @c: the alphabet to be checked
+ *
+ * Return: 1 if the c is lowercase. 0 otherwise
+ */
+
+int is_lower(int c)
+{
+
+	for (c = 97; c <= 122; c++)
+	{
+		if (c > 96 && c <= 122)
+		{
+			return (0);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
