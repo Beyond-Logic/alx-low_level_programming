2f5c3ac1c0f972289c4c4a5fb733edf95d4bb52a
Beyond-Logic <edemaero@gmail.com>
2023-03-21 22:30:08 +0000

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,12 +6,10 @@
   * Return: Always 0 (Success)
 */
 
-void print_alphabet(void){
-	_putchar('\n');
-}
+void print_alphabet(void);
 
 int main(void)
 {
-	print_alphabet();
+	print_alphabet('\n');
 	return (0);
 }
