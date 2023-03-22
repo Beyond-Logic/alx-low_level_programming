fdbe1791851edab65c8d0f5d5b6a4e505c508462
Beyond-Logic <edemaero@gmail.com>
2023-03-22 02:53:46 +0000

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - absolute value of an integer
+ * @a:int a
+ * @b: int b
+ *
+ * Return: return the addition of two integer
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * times_table - Times Table
+ */
+
+void times_table(void)
+{
+
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('i');
+	}
+}
