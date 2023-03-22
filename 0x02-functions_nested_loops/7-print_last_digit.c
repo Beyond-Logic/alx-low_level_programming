90a88457454dda4767ff58c07dad2cd12550ad92
Beyond-Logic <edemaero@gmail.com>
2023-03-22 01:40:35 +0000

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,14 +3,14 @@
 
 /**
  * print_last_digit - print last digit
- * @n: n to be checked
+ * @num: number to be checked
  *
  * Return: the value of the last digit
  */
 
-int print_last_digit(int n)
+int print_last_digit(int num)
 {
-	int lastDigit = abs(n) % 10;
+	int lastDigit = abs(num % 10);
 
 	return (lastDigit);
 }
