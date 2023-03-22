62778361a930c13206f3fe8bc636da6e712e8b2e
Beyond-Logic <edemaero@gmail.com>
2023-03-22 02:28:38 +0000

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,15 +6,19 @@
 
 void jack_bauer(void)
 {
-	int h;
-	int m;
+	int h, m;
 
-	for (m = 0; m <= 59; m++)
+	for (h = 0; h <= 23; h++)
 	{
 
-		for (h = 0; h <= 23; h++)
+		for (m = 0; m <= 59; h++)
 		{
-			_putchar(h);
+			_putchar(h / 10 + '0');
+			_putchar(h % 10 + '0');
+			_putchar(':');
+			_putchar(m / 10 + '0');
+			_putchar(m % 10 + '0');
+			_putchar('\n');
 		}
 	}
 }
