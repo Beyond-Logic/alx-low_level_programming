76a047b5db1db68d8df39a6f9afc3c70fef11e57
Beyond-Logic <edemaero@gmail.com>
2023-03-22 00:23:33 +0000

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - absolute value of an integer
+ * @int: the int to be checked
+ *
+ * Return: absolute value of an integer
+ */
+
+
+int _abs(int)
+{
+	return (abs(int));
+}
