8ff70b7472c4e6d7c01b1040421dd5b74752de0f
Beyond-Logic <edemaero@gmail.com>
2023-03-25 16:11:49 +0000

    task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,13 +1,38 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_number -  print squares of #
+ * print_number -  print an integer #
  * @n: number to print
  */
 
 void print_number(int n)
 {
 
-	return (_putchar(n));
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	int temp = n;
+
+	int num_digits = 0;
+
+	do {
+		temp /= 10;
+		num_digits++;
+	}
+	while
+	{
+		(temp != 0);
+	}
+
+	for (int i = num_digits - 1; i >= 0; i--)
+	{
+		int digit = (n / (int)pow(10, i)) % 10;
+
+		_putchar(digit + '0');
+	}
 
 }
