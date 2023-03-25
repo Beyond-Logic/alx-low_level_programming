ba01b2f0555fe5431bccb6b2003b2ba61472dbe9
Beyond-Logic <edemaero@gmail.com>
2023-03-25 13:14:19 +0000

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,16 +11,18 @@ void print_line(int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	if (n == 0)
 	{
-		_putchar('_');
-
-		if (n == 0)
-		{
-			_putchar('\n');
-		}
+		_putchar('\n');
 	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
 
-	_putchar('\n');
+		_putchar('\n');
+	}
 }
 
