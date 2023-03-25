6077ac89e7a504ac153ec2aac7e8ac11fda118ae
Beyond-Logic <edemaero@gmail.com>
2023-03-25 11:37:02 +0000

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
-  * main - Entry point
-  * Return: 0
+  * _isupper - check for upper case character
+  * @c: the character to be checked
+  *
+  * Return: 1 if c is uppercase otherwise 0
 */
 
 int _isupper(int c)
 {
-	if(c >= 65 && c <= 90)
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
