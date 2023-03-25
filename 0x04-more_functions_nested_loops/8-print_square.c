807effd2ccc4d71d5fab12ff735b9bc6d947e0f1
Beyond-Logic <edemaero@gmail.com>
2023-03-25 13:52:20 +0000

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square -  print squares of #
+ * @size: number of times # should be printed
+ */
+
+void print_square(int size)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		for (i = 0 i < size; i++)
+		{
+			_putchar('#');
+		}
+		_putchar('#');
+	}
+
+	_putchar('\n');
+}
