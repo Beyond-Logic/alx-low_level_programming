374907a1c12b79bc343dc972fd1082deed034b77
Beyond-Logic <edemaero@gmail.com>
2023-03-25 11:44:22 +0000

    task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,4 +1,21 @@
 #include "main.h"
 
 /**
+ * _isdigit - check for digit
+ * @c: the character to be checked
+ *
+ * Return: 1 if c is uppercase otherwise 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
 
