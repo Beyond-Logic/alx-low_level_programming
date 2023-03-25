8f37e08aaddc55c9e81471d002ad472be755d642
Beyond-Logic <edemaero@gmail.com>
2023-03-25 13:55:02 +0000

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,16 +7,16 @@
 
 void print_square(int size)
 {
-	int i;
+	int i, j;
 
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i <= size; i++)
 	{
-		for (i = 0 i < size; i++)
+		for (j = 0 j < size; j++)
 		{
 			_putchar('#');
 		}
