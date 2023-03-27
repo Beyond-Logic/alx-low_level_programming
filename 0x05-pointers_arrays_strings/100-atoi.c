f7544d873c408b907421ae88881a6ce86a337ad2
Beyond-Logic <edemaero@gmail.com>
2023-03-27 19:52:19 +0000

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -17,7 +17,7 @@ int _atoi(char *s)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 
-		if ((s[i] >= 0) && (s[i] <= 9))
+		if ((s[i] >= '0') && (s[i] <= '9'))
 		{
 			answer = answer * 10 + (s[i] - '0');
 		}
