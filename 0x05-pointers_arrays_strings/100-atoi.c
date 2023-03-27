6a36efca50e01f55e55497bac36820d2504d5767
Beyond-Logic <edemaero@gmail.com>
2023-03-27 19:44:10 +0000

    task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer
+ * @s: s
+ * Return: converted string
+ */
+
+int _atoi(char *s)
+{
+	int answer = 0;
+
+	int sign = 1;
+
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+		if ((s[i] >= 0) && (s[i] <= 9))
+		{
+			answer = answer * 10 + (s[i] - '0';
+		}
+		else if (s[i] == '+')
+		{
+		}
+		else if (s[i] == '-')
+		{
+			sign = -1;
+		}
+		else
+		{
+			break;
+		}
+
+	return (answer * sign);
+}
