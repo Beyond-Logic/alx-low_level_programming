b7302b059d5e036904d7cb6fea0b15dfc6de4636
Beyond-Logic <edemaero@gmail.com>
2023-03-27 15:18:51 +0000

    task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _strlen - string length
@@ -9,5 +8,12 @@
 
 int _strlen(char *s)
 {
-	return (strlen(*s));
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
 }
