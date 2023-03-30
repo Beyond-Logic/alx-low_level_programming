2872b094395082790e1c1b4121a9ac791987bbd0
Beyond-Logic <edemaero@gmail.com>
2023-03-30 23:37:30 +0000

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcmp - string compare
+ * @s1: dest
+ * @s2: src
+ * Return: return the dest
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0')
+	{
+		s2t[i] = s1[i];
+		i++;
+	}
+
+	s2[i] = '\0';
+
+	return (s2);
+}
