d742be6b72049344abfd64ffe399db004e30712a
Beyond-Logic <edemaero@gmail.com>
2023-03-31 00:19:55 +0000

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * leet - leet
+ * @c: c
+ * Return: return
+ */
+
+char *leet(char *c);
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		c[i] = toupper(c[i]);
+	}
+
+	return (c);
+}
