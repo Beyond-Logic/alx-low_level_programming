f69dee9b8bd5d0234e541b46a9ec25bce0dc93d4
Beyond-Logic <edemaero@gmail.com>
2023-03-31 00:11:56 +0000

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * cap_string - cap_string
+ * @str: str
+ * Return: return
+ */
+
+char *cap_string(char str);
+{
+	int i = 0;
+
+	str[i] = toupper(str[i]);
+
+	i++;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i - 1] == ' ')
+		{
+			str[i] = toupper(str[i]);
+		}
+
+		i++;
+	}
+
+	return (str);
+}
