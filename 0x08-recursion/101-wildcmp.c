8cc1d0b9336c285ab75268b2dcb7a72260d50e38
Beyond-Logic <edemaero@gmail.com>
2023-04-06 22:17:04 +0000

    task 8

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * wildcmp - Compare two string
+ * @s1: s1
+ * @s2: s2
+ * Return: 1 or 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == *s2)
+	{
+		return (1);
+	}
+
+	return (0);
+}
