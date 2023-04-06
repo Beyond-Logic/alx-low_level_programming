6580debe4b53c21cb6312d078006dfa1a41f5c91
Beyond-Logic <edemaero@gmail.com>
2023-04-06 22:08:22 +0000

    task 7

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * is_palindrome - Palindrome
+ * @s: s
+ * Return: Palindrome
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s != '\0')
+	{
+		temp[] = *s;
+
+		if (*s == temp)
+		{
+			return (1);
+		}
+
+		return (0);
+	}
+}
