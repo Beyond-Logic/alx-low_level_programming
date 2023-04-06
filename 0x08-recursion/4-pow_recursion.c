fda4c0e76d178f6ff0c0564ad1c080575e6e974d
Beyond-Logic <edemaero@gmail.com>
2023-04-06 16:39:08 +0000

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <math.h>
+
+/**
+  * _pow_recursion - POW Recursion
+  *@x: x
+  *@y: y
+  * Return: Pow Recursion
+  */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	return (n * _pow_recursion(pow(x, y) - 1));
+}
