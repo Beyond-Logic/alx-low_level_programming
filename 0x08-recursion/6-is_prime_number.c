07ddf838a6b5674b74ed4d82bccad457ea38ed62
Beyond-Logic <edemaero@gmail.com>
2023-04-06 21:20:33 +0000

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * is_prime_number - Prime Number
+  *@n: n
+  * Return: Pow Recursion
+  */
+
+int is_prime_number(int n)
+{
+	if (n % 2 == 0)
+	{
+		return (1);
+	}
+
+	return (0);
+}
