b1e194144ab800376736a6345ee3672a77f20b2c
Beyond-Logic <edemaero@gmail.com>
2023-04-06 16:12:34 +0000

    task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * factorial - factorial
+  *@n: n
+  * Return: Factorial of a number
+  */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	return (n * factorial(n - 1));
+}
