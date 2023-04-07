310f5c7932b2fc9fba552cfb27a0755bf8123bac
Beyond-Logic <edemaero@gmail.com>
2023-04-07 14:02:26 +0000

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
   * main - Entry point
@@ -7,13 +8,15 @@
   * Return: Return 0;
   */
 
-int main(int argc, __attribute__((unused)) char *argv[])
+int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+
+	int i, sum = 0, sum1;
 
 	for (i = 0; i < argc; i++)
 	{
 		sum = sum + i;
+		sum1 = sum1 + atoi(argv[i]);
 	}
 
 	printf("%d\n", sum);
