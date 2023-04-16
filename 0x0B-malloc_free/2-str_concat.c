8de433b164d8904a9a7b2668655639fc447553e6
Beyond-Logic <edemaero@gmail.com>
2023-04-16 19:11:22 +0000

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - str concat
+ * @s1: s1
+ * @s2: s2
+ * Return: Return
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int *ptr1, *ptr2;
+
+	ptr1 = malloc(sizeof(*s1));
+	ptr2 = malloc(sizeof(*s2));
+
+	if (ptr1 == NULL || ptr2 == NULL)
+	{
+		return (0);
+	}
+
+	if (*s1 == NULL || *s2 == NULL)
+	{
+		printf(" ");
+	}
+
+	return (*s1 + *s2);
+}
