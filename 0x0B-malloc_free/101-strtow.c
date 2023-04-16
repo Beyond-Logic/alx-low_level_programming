385848833ce6b74d63a5de7b532bc935d4dfc5bc
Beyond-Logic <edemaero@gmail.com>
2023-04-16 20:16:56 +0000

    task 6

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * strtow - str
+ * @str: str
+ * Return: Return
+ */
+
+char **strtow(char *str)
+{
+	if (str == NULL || str == "")
+	{
+		return (NULL);
+	}
+
+	return (0);
+}
