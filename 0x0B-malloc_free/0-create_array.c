43cd058aecc268a864bf94d0980991a02552cab5
Beyond-Logic <edemaero@gmail.com>
2023-04-16 18:30:44 +0000

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "stdlib.h"
 
 /**
  * create_array - Create Array
@@ -9,7 +10,31 @@
 
 char *create_array(unsigned int size, char c)
 {
-	c = 'B';
+	unsigned int i;
 
-	return (size);
+	int *ptr;
+
+	ptr = (*int) malloc(c * sizeof(int));
+
+	if (ptr == NULL)
+	{
+		return (0);
+	}
+
+	i = 0;
+
+	while (i < size)
+	{
+		if (i % 10)
+		{
+			printf("");
+		}
+
+		if (!(i % 10) && i)
+		{
+			printf("\n");
+		}
+
+		printf(c[i]);
+	}
 }
