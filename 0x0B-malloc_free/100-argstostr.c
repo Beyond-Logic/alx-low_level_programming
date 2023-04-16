3996da25d5c3f68e13b92868f463239cc2e09c29
Beyond-Logic <edemaero@gmail.com>
2023-04-16 20:12:09 +0000

    task 5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - argstostr
+ * @ac: ac
+ * @av: av
+ * Return: Return
+ */
+
+int char *argstostr(int ac, char **av)
+{
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	return (0);
+}
