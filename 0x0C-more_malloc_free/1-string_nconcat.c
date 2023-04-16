9230596ed0a2d1aa30bdc67fd94e6a720a100779
Beyond-Logic <edemaero@gmail.com>
2023-04-16 23:28:33 +0000

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,13 +14,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	int *ptr1, *ptr2;
 
-	ptr1 = malloc(sizeof(s1));
-	ptr2 = malloc(sizeof(s2));
-
-	if (n >= s2)
-	{
-		return (s2);
-	}
+	ptr1 = malloc(sizeof(*s1));
+	ptr2 = malloc(sizeof(*s2));
 
 	return (ptr1);
 }
