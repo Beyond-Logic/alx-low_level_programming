7d6534cc00b7a04e79270e5283cbb183e9d58cd8
Beyond-Logic <edemaero@gmail.com>
2023-04-16 23:57:19 +0000

    task 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * array_range - array range
+ * @min: min
+ * @max: max
+ * Return: Return
+ */
+
+int *array_range(int min, int max)
+{
+	int *ptr;
+
+	ptr = malloc(sizeof(min, max));
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	return (ptr);
+}
