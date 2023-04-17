46d8b36f0a459d421907e11a31e2f27a7c9d39f7
Beyond-Logic <edemaero@gmail.com>
2023-04-17 22:21:22 +0000

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef dog
+#define dog
 
 /**
   * struct dog - Dog
@@ -10,11 +10,11 @@
   * Description: Dog with name, age, and owner
   */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 }
 
-#endif /* DOG_H */
+#endif /* dog */
