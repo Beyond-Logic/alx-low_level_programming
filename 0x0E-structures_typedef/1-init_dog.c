e3625353f6285b94e15d022eb18f7fd3d6b62509
Beyond-Logic <edemaero@gmail.com>
2023-04-17 22:55:54 +0000

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * init_dog - Dog
+  * @d:  dog
+  * @name: name
+  * @age: age
+  * @owner: owner
+  *
+  * Description: Dog with name, age, and owner
+  */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = "my_dog";
+	name = "Simba";
+	age = 2;
+	owner = ' ';
+}
