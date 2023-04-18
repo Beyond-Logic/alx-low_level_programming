6d0d502748ba197e091dd2f7c691e2900103864c
Beyond-Logic <edemaero@gmail.com>
2023-04-18 11:44:07 +0000

    task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+  * free_dog - Free Dog
+  * @d:  dog
+  *
+  */
+
+void free_dog(dog_t *d)
+{
+	dog_t *d = malloc(sizeof(dog_t));
+
+	if (dot_t == NULL)
+	{
+		free(dog_t);
+
+		return (NULL);
+	}
+}
