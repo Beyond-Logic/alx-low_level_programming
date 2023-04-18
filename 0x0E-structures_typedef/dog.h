3be5a0ac4a9e43885bf69aa741cb2ba218b6bbe2
Beyond-Logic <edemaero@gmail.com>
2023-04-18 10:40:57 +0000

    task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,7 +20,12 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
-struct dog *dog_t;
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
 
 
 #endif /* DOG_H */
