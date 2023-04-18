9c66dfa6814e3f86e6a2a036e96c9864bd8cd980
Beyond-Logic <edemaero@gmail.com>
2023-04-18 10:24:11 +0000

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,22 +11,20 @@
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+	if (d != NULL)
 	{
-		return;
-	}
+		if (!d->age || d->owner == NULL)
+		{
+			printf("(nil)");
+		}
 
-	if (!d->age || d->owner == NULL)
-	{
-		printf("(nil)");
-	}
+		if (d->name == NULL)
+		{
+			printf("Name: (nil)");
+		}
 
-	if (d->name == NULL)
-	{
-		printf("Name: (nil)");
+		printf("Name: %s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner);
 	}
-
-	printf("Name: %s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
 }
