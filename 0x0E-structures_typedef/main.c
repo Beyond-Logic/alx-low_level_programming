17c47a0376529dc8ca26cb86c71a2ca86302d893
Beyond-Logic <edemaero@gmail.com>
2023-04-18 01:06:45 +0000

    task 1

--- 0x0E-structures_typedef/main.c
+++ 0x0E-structures_typedef/main.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 #include "dog.h"
 
@@ -9,15 +10,15 @@
 
 int main(void)
 {
-    struct dog my_dog;
+	struct dog my_dog;
 
-    my_dog.name = "Poppy";
-    
-    my_dog.age = 3.5;
-    
-    my_dog.owner = "Bob";
-    
-    print_dog(&my_dog);
-    
-    return (0);
+	my_dog.name = "Poppy";
+
+	my_dog.age = 3.5;
+
+	my_dog.owner = "Bob";
+
+	print_dog(&my_dog);
+
+	return (0);
 }
