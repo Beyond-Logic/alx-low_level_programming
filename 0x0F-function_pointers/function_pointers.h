72c5dc974655c91b58c696a883f6e94dff14b5d1
Beyond-Logic <edemaero@gmail.com>
2023-04-19 22:41:33 +0000

    task 0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,9 @@
-#ifndef FUNCTIONPOINTER_H
-#define FUNCTIONPOINTER_H
+#ifndef FUNCTIONPOINTERS_H
+#define FUNCTIONPOINTERS_H
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif /* FUNCTIONPOINTER_H */
+#endif /* FUNCTIONPOINTERS_H */
