f49a46dd09df916967c5dd700df64dbb4f1af6d3
Beyond-Logic <edemaero@gmail.com>
2023-04-19 22:35:35 +0000

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,6 +10,11 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+
 	for (size_t i = 0; i < size; i++)
 	{
 		action(array[i]);
