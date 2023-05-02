f3ab9cc5f84275011408515d895b3e2aaa0e9305
Beyond-Logic <edemaero@gmail.com>
2023-05-02 16:35:44 +0000

    task 4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,13 +8,13 @@
 
 void free_listint(listint_t *head)
 {
-	free_listint *temp;
+	listint_t *temp;
 
 	while (head != NULL)
 	{
 		temp = head;
 		head = head->next;
-		free(temp->n);
 		free(temp);
+		temp = NULL;
 	}
 }
