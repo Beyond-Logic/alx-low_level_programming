ce9ed668a68dda21908ab776be91549989c127bb
Beyond-Logic <edemaero@gmail.com>
2023-05-02 17:42:28 +0000

    task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -37,7 +37,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		currentindex = currentindex->next;
 	}
 	temp = currentindex->next;
-	current->next = temp->next;
+	currentindex->next = temp->next;
 	free(temp);
 	return (1);
 }
