ff08046c59aed9518c486e7ed736326406572fcf
Beyond-Logic <edemaero@gmail.com>
2023-05-02 16:41:30 +0000

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+  * free_listint2 - free list
+  * @head: pointer to the head of listint_t
+  */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = *head;
+		head = (*head)->next;
+		free(temp);
+	}
+}
