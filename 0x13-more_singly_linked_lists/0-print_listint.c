8ba393da7ff9ac592fc33b848e9e0c2d3cf3877f
Beyond-Logic <edemaero@gmail.com>
2023-05-02 14:57:18 +0000

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+  * print_listint - Print List
+  * @h: pointer to the head of list_t
+  * Return: number of elements in the list (Print List)
+  */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%lu] %s\n", h->len, h->str);
+		}
+
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
