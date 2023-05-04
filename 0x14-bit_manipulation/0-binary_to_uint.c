c0d09062da3ea9f5cb82f47ca1448d03037c6b58
Beyond-Logic <edemaero@gmail.com>
2023-05-04 20:52:34 +0000

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,19 +10,25 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int value = 0;
 
-	if (b == NULL)
+	if (!b)
 	{
-		return (NULL);
+		return (0);
 	}
 
-	for (; *b != '\0'; b++)
+	while (*b)
 	{
 		if (*b != '0' && *b != '1')
 		{
 			return (0);
 		}
 
-		value = value * 2 + (*b - '0');
+		value = value << 1;
+
+		if (*b == '1')
+		{
+			value = value | 1;
+		}
+		b++;
 	}
 
 	return (value);
