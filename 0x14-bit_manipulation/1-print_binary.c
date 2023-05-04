54dfc9d6a61444f4177c73014ed7e3c87fb2fae5
Beyond-Logic <edemaero@gmail.com>
2023-05-04 21:02:10 +0000

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_binary - Print Binary of a Number
+ * @n: n
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int m = 1UL << (sizeof(unsigned long int));
+
+	int bit;
+
+	while (m)
+	{
+		bit = (n & m) ? 1 : 0;
+		_putchar(bit + '0');
+		m >>= 1;
+	}
+}
