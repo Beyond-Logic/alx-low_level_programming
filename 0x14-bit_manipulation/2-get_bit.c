8bae94826e17384ab59f3a7098383aeb8c73c16c
Beyond-Logic <edemaero@gmail.com>
2023-05-04 21:16:07 +0000

    task 1

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * get_bit - Get Bit at Given Index
+  *@n: n
+  *@index: index
+  * Return: Return
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+	{
+		return (-1);
+	}
+
+	return ((n >> index) & 1);
+}
